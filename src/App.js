fbfb2463654ceb9b0dd34df0321014cd7999ce8d
leejaa <jahun135@hanmail.net>
2019-05-01 19:09:54 +0900

    second

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import UsePromiseSample from './UsePromiseSample';
+import Counter from './Counter';
+import Info from './Info';
+import Average from './Average';
+
+const App = () => {
+  return <div><UsePromiseSample /><Counter /><Info /><Average /></div>;
+};
+
+export default App;
